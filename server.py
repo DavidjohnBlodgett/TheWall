e8ca62c1870a5e2da1db5991cba92e19ce0aa3fc
Branaman <Mbranaman89@gmail.com>
2017-08-09 18:16:07 -0700

    added server.py

---
+++ server.py
@@ -0,0 +1,7 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+app.run(debug=True)
